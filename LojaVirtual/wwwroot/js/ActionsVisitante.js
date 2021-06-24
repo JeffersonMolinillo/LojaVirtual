65b52c50f99a511acef0248e258f1030e88b04ad
JeffersonMolinillo <40474758+JeffersonMolinillo@users.noreply.github.com>
2021-06-23 23:54:58 -0300

    commit

--- LojaVirtual/wwwroot/js/ActionsVisitante.js
+++ LojaVirtual/wwwroot/js/ActionsVisitante.js
@@ -107,74 +107,87 @@ function AtualizarQuantidadeEValor(produto) {
 
     var resultado = produto.valorUnitario * produto.quantidadeProdutoCarrinhoNova;
     produto.campoValor.text(NumberToReal(resultado));
+    AtualizarSubtotal();
+
 }
 
+function AtualizarSubtotal() {
+    var Subtotal = 0;
+    var TagsComPrice = $(".price");
+    TagsComPrice.each(function () {
+        var ValorReais = parseFloat($(this).text().replace("R$", "").replace(".", "").replace(" ", "").replace(",", "."));
+
+        Subtotal += ValorReais;
+
+    })
+    $(".subtotal").text(NumberToReal(Subtotal));
+}
 
 
 function MudarImagePrincipalProduto() {
-    $(".img-small-wrap img").click(function () {
-        var Caminho = $(this).attr("src");
-        $(".img-big-wrap img").attr("src", Caminho);
-        $(".img-big-wrap a").attr("href", Caminho);
-    });
-}
-function MoverScrollOrdenacao() {
-    if (window.location.hash.length > 0) {
-        var hash = window.location.hash;
-        if (hash == "#posicao-produto") {
-            window.scrollBy(0, 473);
+            $(".img-small-wrap img").click(function () {
+                var Caminho = $(this).attr("src");
+                $(".img-big-wrap img").attr("src", Caminho);
+                $(".img-big-wrap a").attr("href", Caminho);
+            });
         }
-    }
-}
+function MoverScrollOrdenacao() {
+            if (window.location.hash.length > 0) {
+                var hash = window.location.hash;
+                if (hash == "#posicao-produto") {
+                    window.scrollBy(0, 473);
+                }
+            }
+        }
 
 
 function MudarOrdenacao() {
-    $("#ordenacao").change(function () {
-        //TODO - Redirecionar para a tela Home/Index passando as QueryString de Ordenação e mantendo a Pagina e a pesquisa.
-        var Pagina = 1;
-        var Pesquisa = "";
-        var Ordenacao = $(this).val();
-        var Fragmento = "#posicao-produto";
+            $("#ordenacao").change(function () {
+                //TODO - Redirecionar para a tela Home/Index passando as QueryString de Ordenação e mantendo a Pagina e a pesquisa.
+                var Pagina = 1;
+                var Pesquisa = "";
+                var Ordenacao = $(this).val();
+                var Fragmento = "#posicao-produto";
 
-        var QueryString = new URLSearchParams(window.location.search);
-        if (QueryString.has("pagina")) {
-            Pagina = QueryString.get("pagina");
+                var QueryString = new URLSearchParams(window.location.search);
+                if (QueryString.has("pagina")) {
+                    Pagina = QueryString.get("pagina");
+                }
+                if (QueryString.has("pesquisa")) {
+                    Pesquisa = QueryString.get("pesquisa");
+                }
+                if ($("#breadcrumb").length > 0) {
+                    Fragmento = "";
+                }
+
+                var URL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+
+                var URLComParametros = URL + "?pagina=" + Pagina + "&pesquisa=" + Pesquisa + "&ordenacao=" + Ordenacao + Fragmento;
+                window.location.href = URLComParametros;
+
+            }
+
+
+            )
+        };
+
+
+
+
+
+    /*--------------------------------------------------*/
+
+
+    class ProdutoQuantidadeEValor {
+        constructor(produtoId, quantidadeEstoque, valorUnitario, campoQuantidadeProdutoCarrinho, campoValor, quantidadeProdutoCarrinhoAntiga, quantidadeProdutoCarrinhoNova) {
+            this.produtoId = produtoId;
+            this.quantidadeEstoque = quantidadeEstoque;
+            this.valorUnitario = valorUnitario;
+
+            this.quantidadeProdutoCarrinhoAntiga = quantidadeProdutoCarrinhoAntiga;
+            this.quantidadeProdutoCarrinhoNova = quantidadeProdutoCarrinhoNova;
+
+            this.campoQuantidadeProdutoCarrinho = campoQuantidadeProdutoCarrinho;
+            this.campoValor = campoValor;
         }
-        if (QueryString.has("pesquisa")) {
-            Pesquisa = QueryString.get("pesquisa");
-        }
-        if ($("#breadcrumb").length > 0) {
-            Fragmento = "";
-        }
-
-        var URL = window.location.protocol + "//" + window.location.host + window.location.pathname;
-
-        var URLComParametros = URL + "?pagina=" + Pagina + "&pesquisa=" + Pesquisa + "&ordenacao=" + Ordenacao + Fragmento;
-        window.location.href = URLComParametros;
-
     }
-
-
-    )
-};
-
-
-
-
-
-/*--------------------------------------------------*/
-
-
-class ProdutoQuantidadeEValor {
-    constructor(produtoId, quantidadeEstoque, valorUnitario, campoQuantidadeProdutoCarrinho, campoValor, quantidadeProdutoCarrinhoAntiga, quantidadeProdutoCarrinhoNova) {
-        this.produtoId = produtoId;
-        this.quantidadeEstoque = quantidadeEstoque;
-        this.valorUnitario = valorUnitario;
-
-        this.quantidadeProdutoCarrinhoAntiga = quantidadeProdutoCarrinhoAntiga;
-        this.quantidadeProdutoCarrinhoNova = quantidadeProdutoCarrinhoNova;
-
-        this.campoQuantidadeProdutoCarrinho = campoQuantidadeProdutoCarrinho;
-        this.campoValor = campoValor;
-    }
-}
